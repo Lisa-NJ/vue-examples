df49e1386ea362deccd3a332d267d635f1a73516
Lisa Huang <LisaHuangAde19@gmail.com>
2022-10-10 23:06:44 +1030

    cli

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+// 项目入口文件
 
+import Vue from 'vue'
+// 引入 App
+import App from './App.vue'
+// 关闭 Vue 的生产提示
 Vue.config.productionTip = false
 
+// 创建 Vue 实例对象 vm
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  // 下面这一行以后解释，将 App 放入容器中
+  render: h => h(App)
+})
