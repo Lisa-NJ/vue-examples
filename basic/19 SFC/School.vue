2b3c5e35f69e24932a758ba4543f95f93e141234
Lisa Huang <LisaHuangAde19@gmail.com>
2022-10-10 19:59:40 +1030

    SFC

---
+++ basic/19 SFC/School.vue
@@ -0,0 +1,24 @@
+ <template>
+  <div class="demo">
+    <h2>school name: {{ name }}</h2>
+    <h2>school address: {{ address }}</h2>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "atGuigu",
+      address: "Beijing",
+    };
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+</style>
